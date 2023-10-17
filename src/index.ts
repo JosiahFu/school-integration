9bf81fc6e1eef615e8a3213bb375e0a10da3f702
JosiahFu <josiahfu@gmail.com>
2023-10-17 12:06:19 -0700

    Add ESLint

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,8 @@ const { App } = bolt;
 
 // Initializes your app with your bot token and signing secret
 const app = new App({
-  token: process.env.SLACK_BOT_TOKEN,
-  signingSecret: process.env.SLACK_SIGNING_SECRET
+    token: process.env.SLACK_BOT_TOKEN,
+    signingSecret: process.env.SLACK_SIGNING_SECRET
 });
 
 // Start your app
