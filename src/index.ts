4f0313615f1aaec278c0165645cfbf8e60a711d6
JosiahFu <josiahfu@gmail.com>
2023-10-17 12:26:25 -0700

    Listening to messages

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,18 @@ import 'dotenv/config';
 import bolt from '@slack/bolt';
 const { App } = bolt;
 
-// Initializes your app with your bot token and signing secret
+// Initializes your app in socket mode with your app token and signing secret
 const app = new App({
     token: process.env.SLACK_BOT_TOKEN,
-    signingSecret: process.env.SLACK_SIGNING_SECRET
+    signingSecret: process.env.SLACK_SIGNING_SECRET,
+    socketMode: true,
+    appToken: process.env.SLACK_APP_TOKEN,
+});
+
+// Listens to incoming messages that contain "hello"
+app.message('hello', async ({ message, say }) => {
+    // say() sends a message to the channel where the event was triggered
+    await say(`Hey there <@${'user' in message ? message.user : ''}>!`);
 });
 
 // Start your app
